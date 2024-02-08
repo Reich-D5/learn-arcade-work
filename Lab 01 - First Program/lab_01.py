3543d1a30969eb1a983c13d72da2c51e2efb080e
Reich-D5 <reichd@hartwick.edu>
2024-02-08 08:15:26 -0500

    Update lab_01.py

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,13 @@
 list = [1, 2, 3, 4 ,5]
 
 print(list)
+
+print("Hello World!")
+
+def odd_numbers(list):
+    return [num for num in list if num % 2 !=0]
+
+numbers = [1, 2, 3, 4, 5]
+
+print(odd_numbers(numbers))
+
