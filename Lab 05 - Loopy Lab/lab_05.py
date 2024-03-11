51dbb78b69ddf95ab887c802ca3c875f48076540
Reich-D5 <159147666+Reich-D5@users.noreply.github.com>
2024-03-11 07:40:02 -0400

    loops

--- Lab 05 - Loopy Lab/lab_05.py
+++ Lab 05 - Loopy Lab/lab_05.py
@@ -1,2 +1,111 @@
-for i in range(10):
-    print(i)
+import arcade
+
+
+def draw_section_outlines():
+    arcade.draw_rectangle_outline(150, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(450, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(750, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(1050, 150, 300, 300, arcade.color.BLACK)
+
+    arcade.draw_rectangle_outline(150, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(450, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(750, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(1050, 450, 300, 300, arcade.color.BLACK)
+
+
+def draw_section_1():
+    for row in range(30):
+        for column in range(30):
+            x = 5 + column * 10
+            y = 295 - row * 10
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+
+def draw_section_2():
+    for row in range(30):
+        for column in range(30):
+            x = 305 + column * 10
+            y = 295 - row * 10
+            if column % 2 == 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
+            else:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+
+def draw_section_3():
+    for row in range(30):
+        for column in range(30):
+            x = 605 + column * 10
+            y = 295 - row * 10
+            if row % 2 == 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
+            else:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+
+def draw_section_4():
+    for row in range(30):
+        for column in range(30):
+            x = 905 + column * 10
+            y = 295 - row * 10
+            if (row + column) % 2 == 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+            else:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
+
+
+def draw_section_5():
+    for row in range(30):
+        for column in range(30):
+            x = 5 + column * 10
+            y = 595 - row * 10
+            draw_rectangle = column >= 29 - row
+            draw_rectangle and arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+
+def draw_section_6():
+    for row in range(30):
+        for column in range(30):
+            x = 305 + column * 10
+            y = 595 - row * 10
+            draw_rectangle = column <= row
+            draw_rectangle and arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+
+def draw_section_7():
+    for row in range(30):
+        for column in range(30 - row):
+            x = 605 + column * 10
+            y = 595 - row * 10
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+
+def draw_section_8():
+    for row in range(30):
+        for column in range(30 - row):
+            x = 1205 - column * 10
+            y = 595 - row * 10
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+
+def main():
+    arcade.open_window(1200, 600, "lab 05 - Loopy Lab")
+    arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
+
+    arcade.start_render()
+
+    draw_section_outlines()
+    draw_section_1()
+    draw_section_2()
+    draw_section_3()
+    draw_section_4()
+    draw_section_5()
+    draw_section_6()
+    draw_section_7()
+    draw_section_8()
+
+    arcade.finish_render()
+
+    arcade.run()
+
+
+main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,7 +1,67 @@
-print("this is a test...")
+"""for i in range(10):
+    for j in range(10):
+        print("*", end="  ")
+    print()
 
-test = "printing to commit to master"
+for i in range(10):
+    print("*", end=" ")
+print()
 
-print(test)
+for i in range(5):
+    print("*", end=" ")
+print()
 
-print("gitHub let me commit this please")
+for i in range(20):
+    print("*", end=" ")
+print()
+
+for i in range(5):
+    for j in range(10):
+        print("*", end=" ")
+    print()
+
+for i in range(5):
+    for j in range(20):
+        print("*", end=" ")
+    print()
+
+for i in range(10):
+    for j in range(10):
+        print(j, end=" ")
+    print()
+
+for i in range(10):
+    for j in range(i+1):
+        print(j, end=" ")
+    print()
+
+for i in range(10):
+    for j in range(10-i):
+        print(j, end=" ")
+    print()
+"""
+for i in range(10):
+    for j in range(i):
+        print(" ", end=" ")
+    for j in range(10-i):
+        print(j, end=" ")
+    print()
+"""
+for i in range(1, 12):
+    for j in range(1, 12):
+        product = i*j
+        if product < 10:
+            print(" ", end="")
+        if product < 100:
+            print(" ", end="")
+        print(product, end=" ")
+    print()
+
+for i in range(1, 10):
+    for j in range(9-i, 0, -1):
+        print(" ", end=" ")
+    for j in range(i):
+        print(j+1, end=" ")
+    for j in range(j, 0, -1):
+        print(j, end=" ")
+    print()"""
